4fdbbee86f26590618fb0577c6ba21efbe94ce5b
Gytis Razokas <grazokas@gmail.com>
2025-02-23 22:58:47 +0200

    addition function works with 2 inputs only, no memory yet

--- calculator.py
+++ calculator.py
@@ -11,30 +11,28 @@
 #     the value stored in memory returns to 0.
 
 class Calculator:
-    def __init__(self, n1, n2):
+    def __init__(self, n1, n2, current = 0):
+        self.current = current
         self.n1 = n1
         self.n2 = n2
-     #   self.current = current
-
+        
     def __str__(self):
         return f"{self.current}"
 
-    def add(self, n1, n2):
-        self.n = n1 + n2
+    def add(self):
+        self.current = self.n1 + self.n2
 
-    def sub(self, n1, n2):
-        self.n1 = n1
-        self.n2 = n2
-        self.n = n1 - n2
+    def sub(self):
+        self.current = self.n1 - self.n2
 
-    def multi(self, n1, n2):
-        self.n = n1 * n2
+    def multi(self):
+        self.current = self.n1 * self.n2
 
-    def div(self, n1, n2):
-        self.n = n1 / n2
+    def div(self):
+        self.current = self.n1 / self.n2
 
-    def root(self, n1, n2):
-        self.n = n1, n2
+    def root(self):
+        self.current = self.n1, self.n2
         
         #  memory
     @property
@@ -44,13 +42,61 @@ class Calculator:
 
     # take user inputs for math and print answers
 def main():
-    n = input("")
+    n = input("").strip()
     if "+" in n:
-        n1, n2 = n.split("+")
-        Calculator.add(n1, n2)
+        addition(n)
     elif "-" in n:
-        n1, n2 = n.split("-")
-        Calculator.sub(n1, n2)
+        subtract(n)
+    elif "*" in n:
+        multiply(n)
+    elif "/" in n:
+        divide(n)
+    elif "root" in n:
+        root(n)
+    else:
+        raise ValueError("Bad input")
+        
+def addition(n):
+    num = []
+    n = n.split("+")
+    for numbers in n:
+        num.append(int(numbers))
+    calc = Calculator(num[0], num[1])
+    calc.add()
+    print(calc)
+
+def subtract(n):
+    num = []
+    for numbers in n:
+        n.split("-")
+        if numbers.isnumeric():
+            num.append(numbers)
+        Calculator.sub(num[0], num[1])
+        
+def multiply(n):
+    num = []
+    for numbers in n:
+        n.split("*")
+        if numbers.isnumeric():
+            num.append(numbers)
+        Calculator.multi(num[0], num[1])
+        
+def divide(n):
+    num = []
+    for numbers in n:
+        n.split("/")
+        if numbers.isnumeric():
+            num.append(numbers)
+        Calculator.div(num[0], num[1])
+        
+def root(n):
+    num = []
+    for numbers in n:
+        n.split("root")
+        if numbers.isnumeric():
+            num.append(numbers)
+        Calculator.root(num[0], num[1])
+        
 
 if __name__ == "__main__":
     main()
