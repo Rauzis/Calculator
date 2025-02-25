8ff6b759b5b7a7e543c2c5ab17f091ba8197f253
Gytis Razokas <grazokas@gmail.com>
2025-02-25 13:55:55 +0200

    reforging for input memory

--- calculator.py
+++ calculator.py
@@ -9,30 +9,31 @@
 #     is calculator.add(2), the result is 2. If calculator.add(2) is performed again, the result is 4.
 #     At this point the value stored in memory is 4. If the calculator memory is then reset,
 #     the value stored in memory returns to 0.
+import numpy as np
+
 
 class Calculator:
-    def __init__(self, n1, n2, current = 0):
+    def __init__(self, n1, current = 0):
         self.current = current
         self.n1 = n1
-        self.n2 = n2
         
     def __str__(self):
         return f"{self.current}"
 
     def add(self):
-        self.current = self.n1 + self.n2
+        self.current = self.current + self.n1
 
     def sub(self):
-        self.current = self.n1 - self.n2
+        self.current = self.current - self.n1
 
     def multi(self):
-        self.current = self.n1 * self.n2
+        self.current = self.current * self.n1
 
     def div(self):
-        self.current = self.n1 / self.n2
+        self.current = self.current / self.n1
 
     def root(self):
-        self.current = self.n1 ** (1 / self.n2)
+        self.current = self.current ** (1 / 2)
         
         #  memory
     @property
@@ -42,69 +43,22 @@ class Calculator:
 
     # take user inputs for math and print answers
 def main():
-    print(3**4)
+    
     while True:
         n = input("").strip()
-    
         if "+" in n:
             addition(n)
             continue
-        elif "-" in n:
-            subtract(n)
-        elif "*" in n:
-            multiply(n)
-        elif "/" in n:
-            divide(n)
-        elif "root" in n:
-            root(n)
         else:
             raise ValueError("Bad input")
         
 def addition(n):
-    num = []
-    n = n.split("+")
-    for numbers in n:
-        num.append(int(numbers))
-    calc = Calculator(num[0], num[1])
+    n = n.replace("+", "")
+    n = int(n)
+    calc = Calculator(n)
     calc.add()
     print(calc)
 
-def subtract(n):
-    num = []
-    n = n.split("-")
-    for numbers in n:
-        num.append(int(numbers))
-    calc = Calculator(num[0], num[1])
-    calc.sub()
-    print(calc)
-        
-def multiply(n):
-    num = []
-    n = n.split("*")
-    for numbers in n:
-        num.append(int(numbers))
-    calc = Calculator(num[0], num[1])
-    calc.multi()
-    print(calc)
-        
-def divide(n):
-    num = []
-    n = n.split("/")
-    for numbers in n:
-        num.append(int(numbers))
-    calc = Calculator(num[0], num[1])
-    calc.div()
-    print(calc)
-        
-def root(n):
-    num = []
-    n = n.split("root")
-    for numbers in n:
-        num.append(int(numbers))
-    calc = Calculator(num[0], num[1])
-    calc.root()
-    print(calc)
-        
 
 if __name__ == "__main__":
     main()
