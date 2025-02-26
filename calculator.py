6d3571b3977d9c54c5620fcf33200ed7dd792e65
Gytis Razokas <grazokas@gmail.com>
2025-02-26 10:14:07 +0200

    all functions updates in to memory, corner cases not implemented

--- calculator.py
+++ calculator.py
@@ -5,17 +5,22 @@
 #     Multiplication / Division.
 #     Take (n) root of a number.
 #     Reset memory. This means the calculator should perform actions using the value stored
-#     in its memory, with memory starting at 0. For example, if the first operation performed
-#     is calculator.add(2), the result is 2. If calculator.add(2) is performed again, the result is 4.
-#     At this point the value stored in memory is 4. If the calculator memory is then reset,
-#     the value stored in memory returns to 0.
-import numpy as np
+    #     in its memory, with memory starting at 0. For example, if the first operation performed
+    #     is calculator.add(2), the result is 2. If calculator.add(2) is performed again, the result is 4.
+    #     At this point the value stored in memory is 4. If the calculator memory is then reset,
+    #     the value stored in memory returns to 0.
 
 
 class Calculator:
-    def __init__(self, n1, current = 0):
+    def __init__(self, n1, current=0):
+        if n1 == "":
+            raise ValueError("missing input")
+        try:
+            self.n1 = float(n1)
+        except ValueError:
+            print("NaN")
+        self.n1 = 0
         self.current = current
-        self.n1 = n1
         
     def __str__(self):
         return f"{self.current}"
@@ -35,28 +40,67 @@ class Calculator:
     def root(self):
         self.current = self.current ** (1 / 2)
         
-        #  memory
-    @property
-    def size(self, current=0):
-        self.current = current
 
 
-    # take user inputs for math and print answers
+
 def main():
-    
+    calc = Calculator(0)
     while True:
-        n = input("").strip()
+        n = input("+, -, *, / or root (ex.: +20), 'done' to end, 'reset' to restart: ").lower().strip()
         if "+" in n:
-            addition(n)
-            continue
-        else:
-            raise ValueError("Bad input")
+            addition(n, calc)
+        elif "-" in n:
+            subtract(n, calc)
+        elif "*" in n:
+            multiply(n, calc)
+        elif "/" in n:
+            divide(n, calc)
+        elif "root" in n:
+            root(n, calc)
+        elif "reset" in n:
+            reset(calc)
+        elif "done" in n:
+            print(f"Final: {calc}")
+            break
         
-def addition(n):
-    n = n.replace("+", "")
-    n = int(n)
-    calc = Calculator(n)
-    calc.add()
+def addition(n, calc):
+    n = n.replace("+", "").strip()
+    if n:
+        calc.n1 = float(n)
+        calc.add()
+        print(calc)
+        
+def subtract(n, calc):
+    n = n.replace("-", "").strip()
+    if n:
+        calc.n1 = float(n)
+        calc.sub()
+        print(calc)
+        
+def multiply(n, calc):
+    n = n.replace("*", "").strip()
+    if n:
+        calc.n1 = float(n)
+        calc.multi()
+        print(calc)
+        
+def divide(n, calc):
+    n = n.replace("/", "").strip()
+    if n:
+        calc.n1 = float(n)
+        calc.div()
+        print(calc)
+        
+def root(n, calc):
+    n = n.replace("root", "").strip()
+    if n:
+        calc.n1 = float(n)
+        calc.root()
+        print(calc)
+
+def reset(calc):
+    calc.n1 = 0
+    calc.multi()
     print(calc)
 
 
