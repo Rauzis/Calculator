88809f0d11d8847e840ea876951a11a5b0b664d0
Gytis Razokas <grazokas@gmail.com>
2025-02-27 13:57:42 +0200

    removed test lines, formated PEP8

--- calculator.py
+++ calculator.py
@@ -8,6 +8,7 @@ class Calculator:
         current (float): The current value of the calculator. Initialized by assigning value `Calculator(0)`.
         n1 (float): The first operand for the operation.
     """
+
     def __init__(self, current):
         """
         Initializes the Calculator with an initial number.
@@ -19,7 +20,7 @@ class Calculator:
             self.current = float(current)
         except TypeError:
             print("Invalid Input")
-        self.current = current #if current is not None else self.n1
+        self.current = current
 
     def __str__(self):
         """
@@ -81,7 +82,8 @@ class Calculator:
                 print("Can't divide by 0")
             else:
                 self.current = self.current / self.n1
-        except EOFError: print(self.current)
+        except EOFError:
+            print(self.current)
 
     def root(self, n1):
         """Takes (n1) root from current value.
@@ -98,85 +100,9 @@ class Calculator:
                 print("Can't extract root from negative numbers")
             else:
                 self.current = self.current ** (1 / self.n1)
-        except EOFError: print(self.current)
+        except EOFError:
+            print(self.current)
 
     def reset(self):
-        """Sets **current** value to zero
-        """
+        """Sets **current** value to zero"""
         self.current = 0
-
-
-    
-
-def main():
-    calc = Calculator(10)
-    while True:
-        n = (
-            input("+, -, *, / or root (ex.: +20), 'done' to end, 'reset' to restart: ")
-            .lower()
-            .strip()
-        )
-        if "+" in n:
-            addition(n, calc)
-        elif "*" in n:
-            multiply(n, calc)
-        elif "/" in n:
-            divide(n, calc)
-        elif "-" in n:
-            subtract(n, calc)
-        elif "root" in n:
-            root(n, calc)
-        elif "reset" in n:
-            reset(calc)
-        elif "done" in n:
-            print(f"Final: {calc}")
-            break
-
-
-def addition(n, calc):
-    n = n.replace("+", "").strip()
-    if n:
-        n = float(n)
-        calc.add(n)
-        print(calc)
-
-
-def subtract(n, calc):
-    n = n.replace("-", "").strip()
-    if n:
-        n = float(n)
-        calc.sub(n)
-        print(calc)
-
-
-def multiply(n, calc):
-    n = n.replace("*", "").strip()
-    if n:
-        n = float(n)
-        calc.multi(n)
-        print(calc)
-
-
-def divide(n, calc):
-    n = n.replace("/", "").strip()
-    if n:
-        n = float(n)
-        calc.div(n)
-        print(calc)
-
-
-def root(n, calc):
-    n = n.replace("root", "").strip()
-    if n:
-        n = float(n)
-        calc.root(n)
-        print(calc)
-
-
-def reset(calc):
-    calc.reset()
-    print(calc)
-
-
-if __name__ == "__main__":
-    main()
